62b7bc32e5ef382853b717da69a0b71ab2b3299c
DELL <DELL@XPS15-LZF>
2018-07-15 00:11:44 +0800

    修改StringManager

--- src/lzf/webserver/util/StringManager.java
+++ src/lzf/webserver/util/StringManager.java
@@ -1,7 +1,9 @@
 package lzf.webserver.util;
 
 import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
 * @author 李子帆
@@ -10,20 +12,28 @@ import java.util.ResourceBundle;
 * @Description 错误信息管理类，每个包对应一个StringManager实例
 */
 public class StringManager {
-	/*
+	
+	private static final Map<Package, StringManager> map = new ConcurrentHashMap<>();
+	
 	private final Locale locale;
 	private final ResourceBundle bundle;
 	
-	private StringManager(String packageName, Locale locale) {
-		this.locale = locale;
-		
-		String bundleName = packageName + ".string";
-		ResourceBundle.getBundle(bundleName, locale);
-		Locale.getDefault();
+	private StringManager(Package p) {
+		locale = Locale.getDefault();
+		String bundleName = p.getName() + ".Strings";
+		bundle = ResourceBundle.getBundle(bundleName, locale);
 	}
 	
-	public static StringManager getStringManager(Class<?> clazz) {
-		String packageName = clazz.getPackage().getName();
-		
-	}*/
+	public String getString(String key) {
+		return bundle.getString(key);
+	}
+	
+	public static StringManager getManager(Class<?> clazz) {
+		Package p = clazz.getPackage();
+		if(!map.containsKey(p)) {
+			StringManager sm = new StringManager(p);
+			return sm;
+		}
+		return map.get(p);
+	}
 }
